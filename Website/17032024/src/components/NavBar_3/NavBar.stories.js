0c80bf46054a76c16cc70e444f7613148e26044b
VeroniaIbrahim <veronia.Ibrahim19@gmail.com>
2024-03-19 17:04:44 +0200

    updates for simulation page

---
+++ Website/17032024/src/components/NavBar_3/NavBar.stories.js
@@ -0,0 +1,14 @@
+import { NavBar_3 } from ".";
+import { NavBar_3 } from "./NavBar_3";
+
+export default {
+  title: "Components/NavBar_3",
+  component: NavBar_3,
+};
+
+export const Default = {
+  args: {
+    className: {},
+    maskGroup: "https://c.animaapp.com/jrXwmMSX/img/mask-group-13@2x.png",
+  },
+};
