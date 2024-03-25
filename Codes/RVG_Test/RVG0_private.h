ea67fc4b6f7f3f37196337993528edb1759cbe2a
Andrew702 <93593809+Andrew702@users.noreply.github.com>
2024-03-25 23:17:36 +0200

    RVG test code

---
+++ Codes/RVG_Test/RVG0_private.h
@@ -0,0 +1,26 @@
+/*
+ * File: RVG0_private.h
+ *
+ * Code generated for Simulink model 'RVG0'.
+ *
+ * Model version                  : 1.2
+ * Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
+ * C/C++ source code generated on : Mon Mar 25 05:56:17 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_RVG0_private_h_
+#define RTW_HEADER_RVG0_private_h_
+#include "rtwtypes.h"
+#include "RVG0_types.h"
+#endif                                 /* RTW_HEADER_RVG0_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
