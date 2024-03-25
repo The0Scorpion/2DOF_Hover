d91ff876ddd6eb4b48ae3764df7e4149ad893899
Andrew702 <93593809+Andrew702@users.noreply.github.com>
2024-03-25 05:20:16 +0200

    Created new model folder

---
+++ Model/RollReducedModel_NEW/JointParameters.m
@@ -0,0 +1,21 @@
+%upper joint parameters
+mass_u = 2.024; %kg
+g = 9.81; %m/s^2
+J_u = 0.049103; %kg.m^2
+L_u = 0.227293; %m
+L_cg_u = 0.066301; %m
+B_u = 0.5;  
+
+%lower joint parameters
+mass_l = 2.495; %kg
+J_l = 0.071440709; %kg.m^2
+L_l = 0.223793; %m
+L_cg_l = 0.102366; %m
+B_l = 0.5;
+
+CurrThrust = 60; %starting thrust in percent power
+MotorSaturation = 40; %saturation limits for motor
+
+%motor + propeller setting
+k = 0.13;
+b = 0.1;
