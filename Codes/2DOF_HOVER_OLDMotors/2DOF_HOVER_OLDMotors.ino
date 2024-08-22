4f458f430869c92307670881ca99a829a50671e4
The0Scorpion <abdobrkat216@yahoo.com>
2024-08-22 22:46:04 +0300

    added old support for motors

---
+++ Codes/2DOF_HOVER_OLDMotors/2DOF_HOVER_OLDMotors.ino
@@ -0,0 +1,95 @@
+#include <WiFi.h>
+#include <EEPROM.h>
+#include "Parameters.h"
+#include "AWS_IOT.h"
+#include "Controller.h"
+
+
+
+#define EnableDebug
+// #define OFFLINE
+
+#ifndef OFFLINE
+#include "OTA.h"
+#endif
+// #define DebugCFMain
+TaskHandle_t TaskHandle_1;
+
+void setup() {
+// calibESCs();
+#ifdef EnableDebug
+  Serial.begin(115200);  // init Serial For debugging
+#endif
+  // initEncoder(0, 0);
+  // Over the air task to allow wireless flash runs on core 1
+  EEPROM.begin(300);
+#ifndef OFFLINE
+  initWiFi();  // Blocking Stop for offline
+
+  xTaskCreatePinnedToCore(
+    OTATask,
+    "OTA Routine",
+    3000,
+    NULL,
+    0,
+    NULL,
+    0);
+
+  // Data Acquisition  Task Run on core 0 for now
+  xTaskCreatePinnedToCore(
+    DataAQU,          // Function to implement the task
+    "Data AQU Loop",  // Name of the task
+    6000,             // Stack size in words
+    NULL,             // Task input parameter
+    0,                // Priority of the task
+    NULL,             // Task handle.
+    0);               // Core where the task should run
+#endif
+  // Main Loop task runs on core 0
+  xTaskCreatePinnedToCore(
+    Control,        // Function to implement the task
+    "Main Loop",    // Name of the task
+    5120,           // Stack size in words
+    NULL,           // Task input parameter
+    9,              // Priority of the task
+    &TaskHandle_1,  // Task handle.
+    0);             // Core where the task should run
+
+#ifdef DebugCFMain
+  initEncoder(0, 0);
+#endif
+  // Initialize the watchdog timer
+
+  esp_task_wdt_init(ResetTime, true);  // Time in seconds
+  esp_task_wdt_add(NULL);              // Add the main task to the watchdog
+  lastMqttMessageTime = millis();
+}
+// #define DebugCFMain
+uint64_t lastHighCurrent;
+void loop() {
+  // Check if the watchdog timer should be reset
+  if (millis() - lastMqttMessageTime < ResetTime * 1000) {
+    // Feed the watchdog
+    esp_task_wdt_reset();
+  }
+  float PitchCurrent = abs(readCurrent(PitchCurrentSense));
+  float RollCurrent = abs(readCurrent(RollCurrentSense));
+#ifdef DebugCurrent
+  Serial.printf("Currents: %f, %f \r\n", PitchCurrent, RollCurrent);
+#endif
+  if (PitchCurrent > CutoffCurrent || RollCurrent > CutoffCurrent) {
+    if (lastHighCurrent > 0) {
+      if (millis() - lastHighCurrent > CutoffCurrentTime) {
+        //Send IOT Warn And stop operation
+        publishWarning("High Current Detected");
+      }
+    } else {
+      publishWarning("High Current Spike Detected");
+      lastHighCurrent = millis();
+    }
+  } else {
+    lastHighCurrent = 0;
+  }
+  vTaskDelay(1000);
+
+}
---
+++ Codes/2DOF_HOVER_OLDMotors/Parameters.h
@@ -0,0 +1,129 @@
+/*
+ * Header file with all the control parameters for the system
+ * Credit: Scorpion
+ * Created: 12/10/2023
+ * Last update: 5/3/2024
+ */
+#pragma once
+
+//#define OnlineControl
+#define CascadedPIDControl
+#include <ArduinoJson.h>
+// Defines the wiring for each major component
+#define X_ENCODER_PIN_A 32
+#define X_ENCODER_PIN_B 33
+#define Y_ENCODER_PIN_A 34
+#define Y_ENCODER_PIN_B 35
+#define FrontMotorPIN 17
+#define RightMotorPIN 16
+#define BackMotorPIN 27
+#define LeftMotorPIN 26
+#define PitchCurrentSense 36 
+#define RollCurrentSense 39
+#define ZERO_CURRENT_VOLTAGE 1.65 // Voltage at zero current
+#define ACS712_SENSITIVITY 66 // mV per Amp for ACS712-30A
+#define CutoffCurrent 20 //Amps
+#define CutoffCurrentTime 5000
+// IoT Parameters
+#define ResetTime 60 //seconds without iot activity to reset after
+#define Send_Period 300 // Unit is ms
+unsigned long lastsent = 0,lastAquired;
+#define SendingQoS 0
+#define ReceiveQoS 0
+#define SendingBufferSize 4096 // Adjust the size according to your needs
+StaticJsonDocument<SendingBufferSize> SendingDoc; 
+StaticJsonDocument<256> DataPacket;
+StaticJsonDocument<512> ParamterObject;
+char SendingString[SendingBufferSize];  // Adjust the size according to your needs
+char WarnString[500];  // Adjust the size according to your needs
+bool Readytosend = false;  // Send flag, set this to true when ready to send the buffer
+// Rest in Secrets.h
+
+int counta = 0;
+int failcount = 0, failLimit = 200;
+int failed_Trials = 0, fail_TrailLimit = 4;
+int maxStartupSamples = 1000, RunSamples = 2000;
+
+
+// Error corrections for IMU
+#define IMU_XACC_Error  -0.02
+#define IMU_YACC_Error   0.00
+#define IMU_ZACC_Error  -0.03
+#define IMU_XDOT_Error  1.14 * PI / 180
+#define IMU_YDOT_Error  -1.87 * PI / 180
+float xDotIMU, yDotIMU;
+float AccX, AccY, AccZ;
+float xPosIMU = 0;
+float yPosIMU = 0;
+
+// Main loop parameters
+double xSpeed, ySpeed; // Feedback speeds for control (after Fusion)
+unsigned int Sampling_time = 5000;
+double IMU_FusionPrio = 0.02;
+byte ControllerRunning = 1, ESC_Running = 1;
+float xAction, yAction;
+byte Work = 0;
+
+// Parameters for PID control
+float xposkp = 8;   // Proportional gain for X position
+float xposki = 0.5; // Integral gain for X position
+float xposkd = 0;   // Derivative gain for X position
+float xvelkp = 15;  // Proportional gain for X velocity
+float xvelki = 1;   // Integral gain for X velocity
+float xvelkd = 0;   // Derivative gain for X velocity
+
+float yposkp = 10;  // Proportional gain for Y position
+float yposki = 0.5; // Integral gain for Y position
+float yposkd = 0;   // Derivative gain for Y position
+float yvelkp = 13;  // Proportional gain for Y velocity
+float yvelki = 1;   // Integral gain for Y velocity
+float yvelkd = 0;   // Derivative gain for Y velocity
+
+float xposSet = 0;  // Setpoint for X position
+float yposSet = 0;  // Setpoint for Y position
+float xvelSet = 0;  // Setpoint for X velocity
+float yvelSet = 0;  // Setpoint for Y velocity
+float PositionLoopSat = 2; // Saturation limit for position loop
+
+// Limits for PID parameters and setpoints
+float xposkpLimit = 12;  // Limit for X position proportional gain
+float xposkiLimit = 5;   // Limit for X position integral gain
+float xposkdLimit = 5;   // Limit for X position derivative gain
+float xvelkpLimit = 30;  // Limit for X velocity proportional gain
+float xvelkiLimit = 5;   // Limit for X velocity integral gain
+float xvelkdLimit = 5;   // Limit for X velocity derivative gain
+
+float yposkpLimit = 10;  // Limit for Y position proportional gain
+float yposkiLimit = 5;   // Limit for Y position integral gain
+float yposkdLimit = 5;   // Limit for Y position derivative gain
+float yvelkpLimit = 20;  // Limit for Y velocity proportional gain
+float yvelkiLimit = 5;   // Limit for Y velocity integral gain
+float yvelkdLimit = 5;   // Limit for Y velocity derivative gain
+
+float xposSetLimit = 0.3; // Limit for X position setpoint
+float yposSetLimit = 0.3; // Limit for Y position setpoint
+
+// Parameters for ESCs and motors
+#define xmaxMicros 1700
+#define xminMicros 1100
+#define xmaxDeltaMicros 300
+#define xmaxDeltaMicrosStart 400
+int xOpratingpoint = 1400;
+
+#define ymaxMicros 1700
+#define yminMicros 1100
+#define ymaxDeltaMicros 300
+#define ymaxDeltaMicrosStart 350
+int yOpratingpoint = 1400;
+
+// Encoder Parameters
+#define PPR 8000   // Counts per revolution for encoders (PPR*4)
+#define pre 20     // Prescaler for hardware timers
+#define NoP  8     // Number of counts for rolling average for encoder velocity calculation
+unsigned long lastMqttMessageTime;
+volatile long xEncoderCount = 0, yEncoderCount = 0;
+unsigned long xLastSpeedTime, yLastSpeedTime;
+uint64_t SpeedUpdateTime = 20000;
+hw_timer_t *SpeedUpdateTimer = NULL, *IntTimer = NULL;
+volatile int64_t xNext = 0, yNext = 0;
+volatile double xDelta[NoP], yDelta[NoP];
